6e6e57ed853ffd9aeef49cc922fa9cc25d0b99c4
chenuki <135982163+cypherfem@users.noreply.github.com>
2026-08-22 16:59:52 -0400

    adding news pages to app.

---
+++ src/main/java/com/ajith/practice/ApiPractice.java
@@ -0,0 +1,61 @@
+package com.ajith.practice;
+import java.net.http.HttpClient;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ApiPractice {
+    public static void main(String[] args) {
+        String myApikey = System.getenv("OPENROUTER_API_KEY");
+        System.out.println("API key found: " + (myApikey != null));
+
+        HttpClient client = HttpClient.newHttpClient();
+        URI uri = URI.create("https://openrouter.ai/api/v1/chat/completions");
+
+        String json = """
+{
+  "model": "nvidia/nemotron-3-nano-30b-a3b:free",
+  "messages": [
+    {
+      "role": "user",
+      "content": "Analyze this sentence for gender bias. Reply in exactly 4 short lines: Bias: Yes or No, Category: one category, Explanation: one short sentence, Rewrite: one short neutral rewrite. Sentence: women are dramatic and emotional."
+    }
+  ]
+}
+""";
+
+   HttpRequest myRequest = HttpRequest.newBuilder()
+                .uri(uri).timeout(Duration.ofSeconds(30))
+                .header("Authorization","Bearer " + myApikey).header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+try {
+    HttpResponse<String> response =
+                        client.send(
+                                myRequest,
+                                HttpResponse.BodyHandlers.ofString()
+                        );
+    System.out.println("Current Status: " + response.statusCode());
+
+    ObjectMapper Ourmapper = new ObjectMapper();
+    JsonNode root = Ourmapper.readTree(response.body());
+    String AIanswer = root
+            .get("choices")
+            .get(0)
+            .get("message")
+            .get("content")
+            .asText();
+
+    System.out.println("AI answer:");
+    System.out.println(AIanswer);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+}
